d4f74e5c4288e76353b4717e95c4d1ca91fd622b
Jonathon LeVitre <jonathonlevitre@Jonathons-MacBook-Pro.local>
2021-11-16 14:26:50 -0700

    fixed issue wiith post potlucks

--- api/potlucks/potlucks-model.js
+++ api/potlucks/potlucks-model.js
@@ -3,7 +3,7 @@ async function insertPotluck(potluck) {
     // WITH POSTGRES WE CAN PASS A "RETURNING ARRAY" AS 2ND ARGUMENT TO knex.insert/update
     // AND OBTAIN WHATEVER COLUMNS WE NEED FROM THE NEWLY CREATED/UPDATED RECORD
     // UNLIKE SQLITE WHICH FORCES US DO DO A 2ND DB CALL
-    const [newPotluckObject] = await db('potlucks').insert(potluck, ['potluck_id', 'name', 'location', 'time', 'date'])
+    const [newPotluckObject] = await db('potlucks').insert(potluck, ['potluck_id', 'name', 'host', 'location', 'time', 'date'])
     return newPotluckObject // { user_id: 7, username: 'foo', password: 'xxxxxxx' }
   }
   function findBy(filter) {
