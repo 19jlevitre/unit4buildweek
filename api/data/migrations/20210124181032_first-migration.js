258064fe50b9d18a61240cfa406741c0b7143f64
Jonathon LeVitre <jonathonlevitre@Jonathons-MacBook-Pro.local>
2021-11-16 14:20:04 -0700

    changed some things

--- api/data/migrations/20210124181032_first-migration.js
+++ api/data/migrations/20210124181032_first-migration.js
@@ -1,28 +1,53 @@
 exports.up = async (knex) => {
   await knex.schema
-    .createTable('users', (users) => {
-      users.increments('user_id')
-      users.string('username', 200).notNullable()
-      users.string('password', 200).notNullable()
-      users.timestamps(false, true)
-    })
     .createTable('potlucks', (potlucks) => {
       potlucks.increments('potluck_id')
       potlucks.string('name', 200).notNullable()
       potlucks.string('location', 200).notNullable()
       potlucks.string('time', 200).notNullable()
       potlucks.string('date', 200).notNullable()
+      potlucks.string('host', 200).notNullable()
       potlucks.timestamps(false, true)
+    })
+    .createTable('users', (users) => {
+      users.increments('user_id')
+      users.string('username', 200).notNullable()
+      users.string('password', 200).notNullable()
+      users.timestamps(false, true)
+      users.integer('potluck_id')
+      .unsigned()
+      .references('potluck_id')
+      .inTable('potlucks')
+      .onUpdate('RESTRICT')
+      .onDelete('RESTRICT')
     })
     .createTable('items', (items) => {
       items.increments('item_id')
       items.string('name', 200).notNullable()
       
+      
+    })
+    .createTable('users_potlucks', (usersPotlucks) => {
+      usersPotlucks.increments('up_id')
+      usersPotlucks.integer('user_id')
+      .unsigned()
+      .references('user_id')
+      .inTable('users')
+      .onUpdate('RESTRICT')
+      .onDelete('RESTRICT')
+      usersPotlucks.integer('potluck_id')
+      .unsigned()
+      .references('potluck_id')
+      .inTable('potlucks')
+      .onDelete('RESTRICT')
+      .onUpdate('RESTRICT')
     })
 }
 
 exports.down = async (knex) => {
+  await knex.schema.dropTableIfExists('users_potlucks')
   await knex.schema.dropTableIfExists('items')
-  await knex.schema.dropTableIfExists('potlucks')
   await knex.schema.dropTableIfExists('users')
+  await knex.schema.dropTableIfExists('potlucks')
+  
 }
--- api/data/seeds/add-potlucks.js
+++ api/data/seeds/add-potlucks.js
@@ -1,13 +1,13 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('potlucks').truncate()
+  return knex('potlucks').del()
     .then(function () {
       // Inserts seed entries
       return knex('potlucks').insert([
-        {name: 'potluck1', location: 'central park', time:'10:30', date:'9/10/21'},
-        {name: 'potluck2', location: 'bountiful', time:'8am', date:'10/11/12'},
-        {name: 'potluck3', location: 'ohio', time:'dusk', date:'12/22/11'}
+        {name: 'potluck1', host: 'doug', location: 'central park', time:'10:30', date:'9/10/21'},
+        {name: 'potluck2', host: 'tim', location: 'bountiful', time:'8am', date:'10/11/12'},
+        {name: 'potluck3', host: 'rick', location: 'ohio', time:'dusk', date:'12/22/11'}
       ]);
     });
 };
